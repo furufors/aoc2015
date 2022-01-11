bfb45c3230b17c2a342801d976e6857a8e811b25
Fredrik Furufors <furufors@gmail.com>
2022-01-11 18:38:37 +0100

    Added day 3

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -1,4 +1,14 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (nub)
+type XY = (Int, Int)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . nub . foldl takeStep [(0, 0)] . head . lines
+    where
+        takeStep :: [XY] -> Char -> [XY]
+        takeStep (s:ss) '^' = (fst s, snd s + 1):s:ss
+        takeStep (s:ss) 'v' = (fst s, snd s - 1):s:ss
+        takeStep (s:ss) '>' = (fst s + 1, snd s):s:ss
+        takeStep (s:ss) '<' = (fst s - 1, snd s):s:ss
+        takeStep ______ ___ = error "Illegal characther."
--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (nub)
+type XY = (Int, Int)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . nub . twoPaths . head . lines
+    where
+        twoPaths :: [Char] -> [XY]
+        twoPaths l = let santa = [e | (e,i) <- zip l [1..], even i]
+                         robo  = [e | (e,i) <- zip l [1..], odd i]
+                     in foldl takeStep [(0, 0)] santa ++ foldl takeStep [(0, 0)] robo
+
+        takeStep :: [XY] -> Char -> [XY]
+        takeStep (s:ss) '^' = (fst s, snd s + 1):s:ss
+        takeStep (s:ss) 'v' = (fst s, snd s - 1):s:ss
+        takeStep (s:ss) '>' = (fst s + 1, snd s):s:ss
+        takeStep (s:ss) '<' = (fst s - 1, snd s):s:ss
+        takeStep ______ ___ = error "Illegal characther."
