04fd79cef019664d69d1e7bd4bd150e952da97be
Fredrik Furufors <furufors@gmail.com>
2022-01-11 18:15:31 +0100

    Added day 2

--- day02/day2p1.hs
+++ day02/day2p1.hs
@@ -1,4 +1,16 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Control.Parallel.Strategies
+import Data.List.Split (splitWhen)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . parMap rpar (toArea . parseIn) . lines
+    where
+        toArea :: (Int, Int, Int) -> Int
+        toArea (l, w, h) = let a = l*w
+                               b = w*h
+                               c = h*l
+                           in 2*a + 2*b + 2*c + minimum [a,b,c]
+
+        parseIn :: String -> (Int, Int, Int)
+        parseIn s = let (a:b:c:_) = splitWhen (=='x') s in (read a, read b, read c)
--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+
+import Control.Parallel.Strategies
+import Data.List.Split (splitWhen)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . parMap rpar (toRibbon . parseIn) . lines
+    where
+        toRibbon :: (Int, Int, Int) -> Int
+        toRibbon (l, w, h) = let a = l*w
+                                 b = w*h
+                                 c = h*l
+                                 perimeter a b = 2 * (a + b)
+                                 bow = l * w * h
+                             in minimum [perimeter l w, perimeter w h, perimeter h l] + bow
+
+        parseIn :: String -> (Int, Int, Int)
+        parseIn s = let (a:b:c:_) = splitWhen (=='x') s in (read a, read b, read c)
