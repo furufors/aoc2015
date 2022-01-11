6c9c3008e35295dd5e6518f65bde69fdf901db70
Fredrik Furufors <furufors@gmail.com>
2022-01-11 19:35:23 +0100

    Added day 4

--- day04/day4p1.hs
+++ day04/day4p1.hs
@@ -1,4 +1,14 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Char (ord)
+import Data.Digest.Pure.MD5
+import Data.List (isPrefixOf)
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Lazy.Char8 as C8
+
+
 main :: IO ()
-main = undefined
+main = putStrLn . show . fst . head . dropWhile (not . isPrefixOf ("00000") . snd) $ generator
+    where
+        generator :: [(Int, String)]
+        generator = [(i, show . md5 . C8.pack $ "ckczppom" ++ show i) | i <- [1..]]
