908dc5df139e5ee2b13b502fd35e6acfad8d3088
Fredrik Furufors <furufors@gmail.com>
2022-01-11 20:16:31 +0100

    Added day 5

--- day05/day5p1.hs
+++ day05/day5p1.hs
@@ -1,4 +1,15 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (isInfixOf)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter classify . lines
+    where
+        classify :: String -> Bool
+        classify s = hasThreeVowels s && hasRepeating s && noNaughtyStrings s
+
+        hasThreeVowels = (>= 3) . length . filter (`elem` "aeiou")
+
+        hasRepeating s = any id $ zipWith (==) s (tail s)
+
+        noNaughtyStrings s = all (\e -> not $ isInfixOf e s) ["ab", "cd", "pq", "xy"]
--- day05/day5p2.hs
+++ day05/day5p2.hs
@@ -1,4 +1,15 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (isInfixOf)
+import Data.List.Split (chunksOf)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter classify . lines
+    where
+        classify :: String -> Bool
+        classify s = repeatingPairs s && oneSpaced s
+
+        repeatingPairs s | length s < 2 = False
+        repeatingPairs s = isInfixOf (take 2 s) (drop 2 s) || repeatingPairs (tail s)
+
+        oneSpaced s = any id $ zipWith (==) s (tail $ tail s)
