4626e18f8cd8a902ecc6f26868db8299a4c5fedc
Fredrik Furufors <furufors@gmail.com>
2022-01-13 21:42:06 +0100

    Added day 10

--- day10/day10p1.hs
+++ day10/day10p1.hs
@@ -1,4 +1,10 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . length . (\s -> (iterate lookAndSay s) !! 40) . head . lines
+
+lookAndSay :: String -> String
+lookAndSay [    ] = []
+lookAndSay (a:as) = let (same,rest) = span (==a) as
+                        nSame = length same
+                    in show (nSame + 1) ++ [a] ++ lookAndSay rest
