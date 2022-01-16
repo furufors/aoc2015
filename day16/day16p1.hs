df9cf00c89229f3314dfa7b7a873090828791c53
Fredrik Furufors <furufors@gmail.com>
2022-01-16 19:45:30 +0100

    Added day 16

--- day16/day16p1.hs
+++ day16/day16p1.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (isInfixOf)
+
 main :: IO ()
-main = undefined
+main = interact $ head . dropWhile ((/= 3) . length . filter id . knownFacts) . lines
+
+knownFacts :: String -> [Bool]
+knownFacts ps = map (\f -> isInfixOf f ps) facts
+
+facts :: [String]
+facts = ["children: 3","cats: 7","samoyeds: 2","pomeranians: 3","akitas: 0","vizslas: 0","goldfish: 5","trees: 3","cars: 2","perfumes: 1"]
--- day16/day16p2.hs
+++ day16/day16p2.hs
@@ -1,4 +1,32 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (isPrefixOf)
+import Data.List.Split (splitWhen)
+
 main :: IO ()
-main = undefined
+main = interact $ fst . head . dropWhile ((/= 3) . length . filter id . map knownFacts . snd) . map parsein . lines
+
+parsein :: String -> (String, [(String, Int)])
+parsein str = let (sue,rst) = substringSplit ":" $ filter (/= ' ') str
+                  kvs = splitWhen (==',') rst
+              in (sue, (fmap read) <$> map (substringSplit ":") kvs)
+
+knownFacts :: (String, Int) -> Bool
+knownFacts ("children", i) = i == 3
+knownFacts ("cats", i)     = i > 7
+knownFacts ("samoyeds", i) = i == 2
+knownFacts ("pomeranians", i) = i < 3
+knownFacts ("akitas", i) = i == 0
+knownFacts ("vizslas", i) = i == 0
+knownFacts ("goldfish", i) = i < 5
+knownFacts ("trees", i) = i > 3
+knownFacts ("cars", i) = i == 2
+knownFacts ("perfumes", i) = i == 1
+knownFacts (s,_) = error ("Encountered property: " ++ s)
+
+substringSplit :: String -> String -> (String, String)
+substringSplit = substringSplit' ""
+
+substringSplit' :: String -> String -> String -> (String, String)
+substringSplit' buf pf [] = error "No match"
+substringSplit' buf pf s = if isPrefixOf pf s then (buf, drop (length pf) s) else substringSplit' (buf ++ [head s]) pf (tail s)
