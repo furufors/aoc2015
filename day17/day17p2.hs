3210d01e86c36a82c7113798a238fa876aa5ebe2
Fredrik Furufors <furufors@gmail.com>
2022-01-16 21:38:52 +0100

    Added day 17

--- day17/day17p2.hs
+++ day17/day17p2.hs
@@ -1,4 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (group, sort, subsequences)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . head . group . sort . map length . filter ((==150) . sum) . subsequences . map read . lines
