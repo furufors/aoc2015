295b695cf8e17007698709951813164b7059c73e
Fredrik Furufors <furufors@gmail.com>
2022-01-17 21:28:11 +0100

    Added day 19

--- day19/day19p1.hs
+++ day19/day19p1.hs
@@ -1,4 +1,31 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (intercalate, isPrefixOf, nub, sort)
+import Data.List.Split (splitOn)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . nub . sort . create . parsein . lines
+
+create :: ([(String, String)], String) -> [String]
+create ([    ], _____) = []
+create ((r:rs), input) = apply r input ++ create (rs, input)
+
+apply :: (String, String) -> String -> [String]
+apply (f,t) str = [ res
+                  | let others = splitOn f str
+                  , let n = length others
+                  , n > 1
+                  , i <- [1..(n-1)]
+                  , let res = (intercalate f $ take i others) ++ t ++ (intercalate f $ drop i others)
+                  ]
+
+parsein :: [String] -> ([(String, String)], String)
+parsein str = let (calib, input) = head . tail <$> span (/= []) str
+              in (map (substringSplit " => ") calib, input)
+
+substringSplit :: String -> String -> (String, String)
+substringSplit = substringSplit' ""
+
+substringSplit' :: String -> String -> String -> (String, String)
+substringSplit' buf pf [] = error "No match"
+substringSplit' buf pf s = if isPrefixOf pf s then (buf, drop (length pf) s) else substringSplit' (buf ++ [head s]) pf (tail s)
--- day19/day19p2.hs
+++ day19/day19p2.hs
@@ -1,4 +1,22 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List.Split (splitWhen)
+
 main :: IO ()
-main = undefined
+main = interact $ show . formula . toElem . last . lines
+
+toElem :: String -> [String]
+toElem [] = []
+toElem s = ([head s]++(takeWhile (`elem` ['a'..'z']) (tail s))):(toElem (dropWhile (`elem` ['a'..'z']) (tail s)))
+
+{- The number of iterations can be expressed as the below formula Rn, Ar and Y represents cases where the element length grows faster than unit length:
+        Base case: Al => ThF       grows with 1 per iteration
+   Base RnAr case: Al => ThRnFAr   grows with 3 per iteration (1 + Rn + Ar)
+      Y+RnAr case: Ca => SiRnFYFAr grows with 5 per iteration (1 + Rn + Ar + Y + 1) (also multiple Ys, each giving 2 extra elements)
+-}
+formula :: [String] -> Int
+formula s = let al = length s
+                rn = length . filter (=="Rn") $ s
+                ar = length . filter (=="Ar") $ s
+                y  = length . filter (=="Y") $ s
+            in al - rn - ar - 2*y - 1
