da992df4e158fd219cf0fb869fa5d8fe6cb16961
Fredrik Furufors <furufors@gmail.com>
2022-01-18 22:01:06 +0100

    Initiated day 20

--- day20/day20p1.hs
+++ day20/day20p1.hs
@@ -1,4 +1,7 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = putStrLn $ show . (+1) . length . takeWhile ((<36000000) . toCount) $ [1..]
+
+toCount :: Int -> Int
+toCount n = sum [10 * x | x <- [1..n], n `mod` x == 0]
