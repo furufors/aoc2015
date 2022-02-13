a249f7a0a5d98b421bcb64eb0d9aa5d09ab8f142
Fredrik Furufors <furufors@gmail.com>
2022-02-13 14:18:21 +0100

    Added day 24

--- day24/day24p1.hs
+++ day24/day24p1.hs
@@ -1,4 +1,22 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (sortBy, subsequences)
+
+type Weight = Int
+type QE = Int
+
 main :: IO ()
-main = undefined
+main = interact $ show . head . sortBy sortConfigurations . map minWeightQe . group . parsein
+
+sortConfigurations :: (Weight, QE, [Weight]) -> (Weight, QE, [Weight]) -> Ordering
+sortConfigurations (w1, qe1, _) (w2, qe2, _) = compare w1 w2 <> compare qe1 qe2
+
+minWeightQe :: [Weight] -> (Weight, QE, [Weight])
+minWeightQe a = (length a, product a, a)
+
+group :: [Weight] -> [[Weight]]
+group ws = let targetWeight = sum ws `div` 3
+           in [ a | a <- subsequences ws, sum a == targetWeight]
+
+parsein :: String -> [Weight]
+parsein = map read . lines
