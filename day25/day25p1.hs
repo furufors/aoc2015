30e226272eefd7dd3c3623192c4310e74fdd980f
Fredrik Furufors <furufors@gmail.com>
2022-02-13 17:31:14 +0100

    Added day 25

--- day25/day25p1.hs
+++ day25/day25p1.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = putStrLn $ show $ codeNum !! ((toNum 3010 3019) - 1)
+
+toNum :: Int -> Int -> Int
+toNum row column = (sum [0..(row-1)] + 1) + sum (take (column-1) [(row+1), (row + 2)..])
+
+codeNum :: [Integer]
+codeNum = 20151125:(codeNum' 20151125)
+    where
+        codeNum' x = let next = (x * 252533) `mod` 33554393 in next:(codeNum' next)
