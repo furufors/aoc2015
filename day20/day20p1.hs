4e2dff7ef242e0842f0f1c18024c0cca42a4150c
Fredrik Furufors <furufors@gmail.com>
2022-02-16 18:15:13 +0100

    Added day 20

--- day20/day20p1.hs
+++ day20/day20p1.hs
@@ -1,7 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (nub)
 main :: IO ()
-main = putStrLn $ show . (+1) . length . takeWhile ((<36000000) . toCount) $ [1..]
+main = putStrLn $ show . fst . head . dropWhile (not . snd) $ map toCount [2..]
 
-toCount :: Int -> Int
-toCount n = sum [10 * x | x <- [1..n], n `mod` x == 0]
+threshold :: Int
+threshold = 36000000 `div` 10 -- Offset problem by a factor 10
+
+toCount :: Int -> (Int, Bool)
+toCount a = let sqroot = ceiling . sqrt . fromIntegral $ a
+            in (a, (>threshold) . sum . nub . concat $ [ [q, d] | d <- [1..sqroot], let (q,r) = a `divMod` d, r == 0])
--- day20/day20p2.hs
+++ day20/day20p2.hs
@@ -1,4 +1,14 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (nub)
+
 main :: IO ()
-main = undefined
+main = putStrLn $ show . fst . head . dropWhile (not . snd) $ map toCount [2..]
+
+threshold :: Int
+threshold = 36000000 -- Offset problem by a factor 10
+
+toCount :: Int -> (Int, Bool)
+toCount a = let sqroot = ceiling . sqrt . fromIntegral $ a
+                toBool = (>threshold) . (11*) . sum . filter (> (a-1) `div` 50) . nub . concat
+            in (a, toBool $ [ [q, d] | d <- [1..sqroot], let (q,r) = a `divMod` d, r == 0])
